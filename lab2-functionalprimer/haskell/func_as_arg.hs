a1da60067dffc7d1362e0b8e12e06d49a5044b3e
Aland Azad <kurdistan201324@gmail.com>
2024-02-06 08:38:43 +0000

    lab 2

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,8 +1,17 @@
 --create inpFunc
-inpFunc = [1..5] 
+a = 5
+b = 50
+inpFunc = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- applicatorFunc inpFunc s = 
+--     if s=='s' 
+--         then sum inpFunc 
+--     else (sum inpFunc)/b  
+
+applicatorFunc inpFunc s 
+    | s == 's' = sum inpFunc 
+    | otherwise = (sum inpFunc)/b
 
 main = do
     let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -1,11 +1,22 @@
 module Main where
 
-main =
-    do
-    print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
 
-myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+
+main :: IO()
+main = do
+    print (myFunc1 5)
+    print (myFunc2 10)
+    print _3rdFunc
+    print (inc 3)
+
+inc :: Int -> Int
+inc x = x + 1 
+
+myFunc1 :: Int -> Int
+myFunc1 x = x * 10
+
+myFunc2 :: Int -> Int
+myFunc2 x = x * 2
+
+_3rdFunc :: [Char]
+_3rdFunc = "Hello, this is 3rd Func"
